4c8c169ffe930f9f71f908f9833d8199c383b5d4
peihongchen <peihongchen@tencent.com>
2020-07-07 13:25:33 +0800

    198-house-robber

---
+++ leetcode/198-house-robber.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int rob(vector<int> &nums)
+    {
+        if (nums.size() == 0)
+        {
+            return 0;
+        }
+
+        int dp0 = nums[0], dp1 = 0;
+
+        for (size_t i = 1; i < nums.size(); i++)
+        {
+            int tmp = dp1 + nums[i];
+            dp1 = max(dp0, dp1);
+            dp0 = tmp;
+        }
+
+        return max(dp0, dp1);
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution s;
+    vector<int> nums;
+    nums.push_back(2);
+    nums.push_back(7);
+    nums.push_back(9);
+    nums.push_back(3);
+    nums.push_back(1);
+    cout << s.rob(nums) << endl;
+    return 0;
+}
+
+/*
+
+你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+
+给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+
+ 
+
+示例 1：
+
+输入：[1,2,3,1]
+输出：4
+解释：偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
+     偷窃到的最高金额 = 1 + 3 = 4 。
+示例 2：
+
+输入：[2,7,9,3,1]
+输出：12
+解释：偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
+     偷窃到的最高金额 = 2 + 9 + 1 = 12 。
+ 
+
+提示：
+
+0 <= nums.length <= 100
+0 <= nums[i] <= 400
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/house-robber
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+*/
